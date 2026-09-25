2599b76fd833918e0907b4630ec52b4236934ff2
Lisa Liu <lisasiliu@Lisas-Mac-mini.local>
2026-09-24 23:46:07 -0400

    part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -22,15 +22,16 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  s =~ /^[a-z]/i && s =~ /^[^aeiou]/i ? true : false
 end
 
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  return false if s.empty? || s =~ /[^01]/
+  s == '0' || s.end_with?('00')
 end
 
 # Part 3
